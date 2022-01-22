5bc3acc0e923f0c4f8e1ff9308d13f8da440ad4d
Bradley Stouffer <voidmaelstrom@gmail.com>
2022-01-22 10:15:27 -0700

    Retrieve data from mock server

--- public/index.js
+++ public/index.js
@@ -1,9 +1,14 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
 
-    let countValue = 0;
+    const response = await fetch('http://localhost:9001/counter');
+    const responseJSON = await response.json();
+
+    console.log(responseJSON);
+
+    let countValue = JSON.stringify(responseJSON.value);
 
     function increment(){
         countValue++;
